451ca4f107f4e155ea208249f4883e4a7e01653f
keshew <artyom.korotckov2017@yandex.ru>
2025-02-03 18:21:24 +0500

    Initial Commit

---
+++ SugarRush/SugarRushApp.swift
@@ -0,0 +1,17 @@
+//
+//  SugarRushApp.swift
+//  SugarRush
+//
+//  Created by Артём Коротков on 03.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct SugarRushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
